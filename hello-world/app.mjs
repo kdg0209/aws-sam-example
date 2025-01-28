a7ac9b6c6ead1cce68c974a00d3573ec6ec07f58
KDG-IS <s_cova_0418@naver.com>
2025-01-28 14:47:10 +0900

    feat: 노드 모듈 레이어 분리

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -1,8 +1,12 @@
+import { v5 as uuidv5 } from 'uuid';
+
 export const lambdaHandler = async (event, context) => {
+
     const response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'hello world',
+        message: 'hello lambda ?',
+        uuid: uuidv5.URL
       })
     };
 
