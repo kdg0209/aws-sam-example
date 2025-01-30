b9aeccd754198a8390606c80707b01f8651d1f1c
kdg0209 <s_cova_0418@naver.com>
2025-01-30 21:57:44 +0900

    배포 스크립트 수정

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -1,12 +1,18 @@
-import { v5 as uuidv5 } from 'uuid';
+import { v4 } from 'uuid';
+import mysql from 'mysql2';
+import dayjs from 'dayjs';
+import chromium from '@sparticuz/chromium';
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
 
 export const lambdaHandler = async (event, context) => {
-
+    const d = dayjs();
+    
     const response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'hello lambda ?',
-        uuid: uuidv5.URL
+        message: 'example sam with lambda test !!!',
+        uuid: v4(),
+        day: d.format('YYYY-MM-DD HH:mm:ss')
       })
     };
 
